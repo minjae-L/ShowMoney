4350f33e2fea49455d5edb4160633d55f98e480c
이민재 <qlqlzbto@gmail.com>
2024-04-14 17:19:35 +0900

    프로젝트 생성

---
+++ ShowMoney/ShowMoney/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ShowMoney
+//
+//  Created by 이민재 on 4/14/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
