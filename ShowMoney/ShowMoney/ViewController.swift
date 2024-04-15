80ca4c0f0301bdbacc3060cec4c36cc7856f52b0
이민재 <qlqlzbto@gmail.com>
2024-04-15 21:05:26 +0900

    Flowlayout -> CompotionalLayout

--- ShowMoney/ShowMoney/ViewController.swift
+++ ShowMoney/ShowMoney/ViewController.swift
@@ -8,31 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-    private let collectionView = {
-        let flowLayout = UICollectionViewFlowLayout()
-        flowLayout.scrollDirection = .vertical
-        let cv = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
-        cv.translatesAutoresizingMaskIntoConstraints = false
-        cv.backgroundColor = .gray
-        cv.register(CollectionViewCell.self,
-                    forCellWithReuseIdentifier: CollectionViewCell.identifier)
-        cv.register(CollectionHeaderView.self,
-                    forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
-                    withReuseIdentifier: CollectionHeaderView.identifier)
-        return cv
-    }()
+    private var collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
         addView()
+        configureConstraints()
     }
     private func addView() {
-        view.addSubview(collectionView)
-        configureConstraints()
-        collectionView.delegate = self
-        collectionView.dataSource = self
+        makeCollectionView()
+        
     }
     private func configureConstraints() {
+        self.collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
         collectionView.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
         collectionView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
@@ -40,39 +29,110 @@ class ViewController: UIViewController {
     }
 }
 
-extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+// MARK: - CollectionView CompotionalLayout
+extension ViewController {
+    private func makeCollectionView() {
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex,_) -> NSCollectionLayoutSection? in
+            return self.createSection(for: sectionIndex)
+        }
+        self.collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        self.collectionView.backgroundColor = .white
+        
+        registerCells()
+        view.addSubview(collectionView)
+        
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewCell.identifier, for: indexPath) as? CollectionViewCell else { return UICollectionViewCell() }
-        return cell
+    private func registerCells() {
+        self.collectionView.register(CollectionViewCell.self, forCellWithReuseIdentifier: CollectionViewCell.identifier)
+        self.collectionView.register(CollectionHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: CollectionHeaderView.identifier)
     }
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 3
-    }
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        if section == 0 { return CGSize(width: collectionView.layer.frame.width, height: 200) }
-        return CGSizeZero
-    }
-    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        if kind == UICollectionView.elementKindSectionHeader {
-            guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader,
-                                                                               withReuseIdentifier: CollectionHeaderView.identifier,
-                                                                               for: indexPath) as? CollectionHeaderView else { return UICollectionReusableView() }
-            return header
-        } else {
-            return UICollectionReusableView()
+    
+    private func createSection(for sectionIndex: Int) -> NSCollectionLayoutSection {
+        switch sectionIndex {
+        case 0:
+            return createMainSection(weightConstant: sectionIndex)
+        default:
+            return createSubSection(weightConstant: sectionIndex)
         }
     }
-}
-extension ViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 100, height: 100)
+    
+    private func createMainSection(weightConstant: Int) -> NSCollectionLayoutSection {
+        let weight = (Double(weightConstant) * 0.05)
+        
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.2), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(0.07))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        group.interItemSpacing = NSCollectionLayoutSpacing.flexible(10.0)
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 15.0
+        section.contentInsets = .init(top: 0, leading: 0, bottom: 0, trailing: 0)
+        section.orthogonalScrollingBehavior = .paging
+        
+        let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(100))
+        let header = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+        
+        section.boundarySupplementaryItems = [header]
+        
+        return section
     }
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 10
+    
+    private func createSubSection(weightConstant: Int) -> NSCollectionLayoutSection {
+        let weight = (Double(weightConstant) * 0.05)
+        
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(0.2))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        group.interItemSpacing = NSCollectionLayoutSpacing.flexible(10.0)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 15.0
+        
+        return section
     }
 }
 
+// MARK: - CollectionView Datasouce Delegate
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewCell.identifier, for: indexPath) as? CollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        if indexPath.section == 0 {
+            cell.backgroundColor = .red
+        } else {
+            cell.backgroundColor = .green
+        }
+        
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
+                                                                               withReuseIdentifier: CollectionHeaderView.identifier,
+                                                                               for: indexPath) as? CollectionHeaderView else { return UICollectionReusableView() }
+                return header
+        case UICollectionView.elementKindSectionFooter:
+            return UICollectionReusableView()
+        default:
+            print("Header and Footer Error")
+            return UICollectionReusableView()
+        }
+    }
+    
+}
